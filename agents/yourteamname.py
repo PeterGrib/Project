ec74700052433eca28d41600cd8cf218ffd1bb3c
PeterGrib <63427353+PeterGrib@users.noreply.github.com>
2021-12-05 12:06:23 -0500

    Add files via upload

--- agents/yourteamname.py
+++ agents/yourteamname.py
@@ -82,13 +82,14 @@ class Agent(object):
         cust_data = new_buyer_covariates.tolist()
 
 
-        if type(new_buyer_embedding) != list:
-            cust_data.append(self.item0avg)
-            cust_data.append(self.item1avg)
-
-        else:
-            cust_data.append(np.dot(new_buyer_embedding, self.item0_embedding))
-            cust_data.append(np.dot(new_buyer_embedding, self.item1_embedding))
+        # if type(new_buyer_embedding) != list:
+        #     cust_data.append(self.item0avg)
+        #     cust_data.append(self.item1avg)
+        #
+        # else:
+        cust_data.append(np.dot(new_buyer_embedding, self.item0_embedding))
+        cust_data.append(np.dot(new_buyer_embedding, self.item1_embedding))
+        print(cust_data)
 
         item0_prices = np.arange(0,2.22, .75)
         item1_prices = np.arange(0,4,.75)
@@ -102,6 +103,7 @@ class Agent(object):
         max_2 = 0
         for j in item0_prices:
             for k in item1_prices:
+                print(test_array)
                 test_array = cust_data
                 test_array.insert(0, j)
                 test_array.insert(1, k)
